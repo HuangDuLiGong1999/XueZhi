66799c2c929959fc0b99b6af74fda4d6904df942
Krisskr <546628725@qq.com>
2019-11-18 16:21:56 +0800

    server修改

--- code/xuezhi-backend/server/src/main/java/com/xuezhi/server/ServerApplication.java
+++ code/xuezhi-backend/server/src/main/java/com/xuezhi/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.xuezhi.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class DemoApplication {
+public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 
 }
