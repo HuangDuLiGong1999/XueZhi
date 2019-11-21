1aa3c3edb273bbbd604c0e3c449d03f7dc0cadec
Krisskr <546628725@qq.com>
2019-11-21 22:43:33 +0800

    qaservice的eureka注册

--- code/xuezhi-backend/qa_service/src/main/java/com/xuezhi/qa_service/SwaggerConfiguration.java
+++ code/xuezhi-backend/qa_service/src/main/java/com/xuezhi/qa_service/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
 package com.xuezhi.qa_service;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -10,6 +11,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableDiscoveryClient
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
