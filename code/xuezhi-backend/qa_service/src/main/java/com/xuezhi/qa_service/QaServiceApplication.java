6f911c1415d1c1a715e5e230f6bef19789c0c451
Huangjinx <1073130610@qq.com>
2019-11-22 08:28:13 +0800

    更改了相关注解的位置

--- code/xuezhi-backend/qa_service/src/main/java/com/xuezhi/qa_service/QaServiceApplication.java
+++ code/xuezhi-backend/qa_service/src/main/java/com/xuezhi/qa_service/QaServiceApplication.java
@@ -2,7 +2,12 @@ package com.xuezhi.qa_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableDiscoveryClient
+@EnableSwagger2
 @SpringBootApplication
 public class QaServiceApplication {
 
--- code/xuezhi-backend/qa_service/src/main/java/com/xuezhi/qa_service/SwaggerConfiguration.java
+++ code/xuezhi-backend/qa_service/src/main/java/com/xuezhi/qa_service/SwaggerConfiguration.java
@@ -1,17 +1,14 @@
 package com.xuezhi.qa_service;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.Documentation;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableDiscoveryClient
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
