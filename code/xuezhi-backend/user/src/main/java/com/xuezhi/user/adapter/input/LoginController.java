bfb8cdaa160c7a14b515934f7717906c7e82513e
Huangjinx <1073130610@qq.com>
2019-11-22 21:34:27 +0800

    修改了login接口的返回类型

--- code/xuezhi-backend/user/src/main/java/com/xuezhi/user/adapter/input/LoginController.java
+++ code/xuezhi-backend/user/src/main/java/com/xuezhi/user/adapter/input/LoginController.java
@@ -6,6 +6,9 @@ import com.xuezhi.user.domain.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @CrossOrigin
 @RequestMapping(value="/login")
@@ -17,9 +20,26 @@ public class LoginController {
         this.usersApplication = usersApplication;
     }
 
+    /*
     @PostMapping
     public User checkUser(@RequestParam String email, @RequestParam String password){
         return usersApplication.getUserByEmailAndPassword(email, password);
+    }
+
+     */
+
+    @PostMapping
+    public Map<String, Object> check(@RequestParam String email, @RequestParam String password){
+        Map<String, Object> map = new HashMap<>(3);
+        if (!(usersApplication.getUserByEmailAndPassword(email, password) == null)){
+            map.put("status", true);
+            map.put("user", usersApplication.getUserByEmailAndPassword(email, password));
+            return map;
+        }
+        else{
+            map.put("status", false);
+            return map;
+        }
     }
 
 
